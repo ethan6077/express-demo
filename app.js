262ba7902be8bce48b80eb718fa0ff7e13fcff7d
Ethan Zhang <ethan6077@gmail.com>
2021-02-14 22:29:27 +1100

    feat: read JSON from file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "fs";
 
 const app = express();
 const port = 4000;
@@ -7,7 +8,13 @@ app.get("/", (req, res) =>
   res.send(`Express Server is running on PORT: ${port}`)
 );
 
-app.get("/properties", (req, res) => "properties");
+app.get("/test", (req, res) => res.send("Test"));
+
+app.get("/properties", (req, res) => {
+  fs.readFile("./data/properties.json", "utf-8", (err, data) => {
+    res.json(JSON.parse(data));
+  });
+});
 
 app.listen(port, () =>
   console.log(`Express server app is listening on port ${port}!`)
